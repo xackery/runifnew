11fdedd41c94ff21ad06d4c525c2ff2f183c7be3
Xackery <xackery@gmail.com>
2023-09-11 15:20:29 -0700

    Add CICD

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 var (
@@ -19,21 +22,28 @@ func main() {
 
 	cmd := ""
 	url := ""
+	urlPath := ""
 	flag.StringVar(&cmd, "cmd", "", "Command to execute")
 	flag.StringVar(&url, "url", "", "URL to fetch")
+	flag.StringVar(&urlPath, "urlPath", "", "Path to save URL to")
 	flag.Parse()
 
 	if cmd == "" {
 		usage()
 		os.Exit(1)
 	}
-	fmt.Println("Command if true:", cmd)
+	fmt.Println("If TRUE, run:", cmd)
 
 	if url == "" {
 		usage()
 		os.Exit(1)
 	}
-	fmt.Println("URL if false:", url)
+
+	if urlPath == "" {
+		usage()
+		os.Exit(1)
+	}
+	fmt.Println("If FALSE, fetch:", url, "and save to:", urlPath)
 
 	gitPath, err := exec.LookPath("git")
 	if err != nil {
@@ -41,29 +51,78 @@ func main() {
 		os.Exit(1)
 	}
 
-	// get latest head hash
-	result, err := run(exec.Command(gitPath, "rev-parse", "-HEAD"))
+	latestHash, err := run(exec.Command(gitPath, "rev-parse", "HEAD"))
 	if err != nil {
+		fmt.Println("Output:", latestHash)
 		fmt.Println("Error getting git hash:", err)
 		os.Exit(1)
 	}
-	fmt.Println("Git hash:", result)
-
-	//$(eval NEW_HASH := $(shell git log -n 1 --pretty=format:%H -- charcreate/charcreate.yaml))
-
-	for _, folder := range flag.Args() {
-		fmt.Println(folder)
+	if strings.Contains(latestHash, "\n") {
+		latestHash = strings.ReplaceAll(latestHash, "\n", "")
 	}
-	fmt.Println(gitPath)
+	fmt.Println("latest hash:", latestHash)
+
+	isFound := false
+	for _, path := range flag.Args() {
+		hash, err := run(exec.Command(gitPath, "log", "-n 1", "--pretty=format:%H", "--", path))
+		if err != nil {
+			fmt.Println("Output:", hash)
+			fmt.Println("Error getting git hash:", err)
+			os.Exit(1)
+		}
+		if strings.Contains(hash, "\n") {
+			hash = strings.ReplaceAll(hash, "\n", "")
+		}
+		if strings.EqualFold(hash, latestHash) {
+			fmt.Println("TRUE:", path, "was updated, triggering cmd")
+			isFound = true
+			break
+		}
+	}
+	if isFound {
+		result, err := run(exec.Command(cmd))
+		if err != nil {
+			fmt.Println("Output:", result)
+			fmt.Println("Error running cmd:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Output:", result)
+		return
+	}
+	fmt.Println("FALSE: Fetching url:", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching url:", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		fmt.Println("Error fetching url, status code:", resp.StatusCode)
+		os.Exit(1)
+	}
+	fmt.Println("Downloaded url:", url)
+	f, err := os.Create(urlPath)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Saved url to:", urlPath)
 
 }
 
 func usage() {
-	fmt.Println("Usage: runifnew -cmd [build] -url [https://download] [folders...]")
+	fmt.Println("Usage: runifnew -cmd [build] -url [https://download] -urlPath bin/test [folders...]")
 	fmt.Println("This program runs build if the git version on folders... matches the latest commit, otherwise it fetches the download url")
 }
 
 func run(cmd *exec.Cmd) (string, error) {
+	fmt.Println("Executing command:", strings.Join(cmd.Args, " "))
 	buf := &bytes.Buffer{}
 	cmd.Stdout = buf
 	cmd.Stderr = buf
