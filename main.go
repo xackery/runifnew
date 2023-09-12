da2bdf5bbfefcb50e63474c883e80ae024a5d2f8
Xackery Xtal <xackery@gmail.com>
2023-09-11 17:04:51 -0700

    Runifnew is now quiet

--- main.go
+++ main.go
@@ -14,17 +14,24 @@ import (
 
 var (
 	// Version is exported during build
-	Version string
+	Version   string
+	isVerbose bool
 )
 
 func main() {
 	var err error
+
+	if len(os.Args) > 0 && os.Args[1] == "version" {
+		fmt.Println("runifnew version:", Version)
+		os.Exit(0)
+	}
 
 	cmd := ""
 	url := ""
 	urlPath := ""
 	flag.StringVar(&cmd, "cmd", "", "Command to execute")
 	flag.StringVar(&url, "url", "", "URL to fetch")
+	isVerbose = *flag.Bool("v", false, "Verbose output")
 	flag.StringVar(&urlPath, "urlPath", "", "Path to save URL to")
 	flag.Parse()
 
@@ -42,13 +49,12 @@ func main() {
 		usage()
 		os.Exit(1)
 	}
-	fmt.Println("runifnew version:", Version)
-	fmt.Println("If TRUE, run:", cmd)
-	fmt.Println("If FALSE, fetch:", url, "and save to:", urlPath)
+	println("If TRUE, run:", cmd)
+	println("If FALSE, fetch:", url, "and save to:", urlPath)
 
 	gitPath, err := exec.LookPath("git")
 	if err != nil {
-		fmt.Println("git is not installed, please install git and try again")
+		println("git is not installed, please install git and try again")
 		os.Exit(1)
 	}
 
@@ -57,77 +63,78 @@ func main() {
 
 	cmdPath, err := exec.LookPath(cmdBinary)
 	if err != nil {
-		fmt.Println("cmd", cmdBinary, "is not installed, please install cmd and try again")
+		println("cmd", cmdBinary, "is not installed, please install cmd and try again")
 		os.Exit(1)
 	}
 
 	latestHash, err := run(exec.Command(gitPath, "rev-parse", "HEAD"))
 	if err != nil {
-		fmt.Println("Output:", latestHash)
-		fmt.Println("Error getting git hash:", err)
+		println("Output:", latestHash)
+		println("Error getting git hash:", err)
 		os.Exit(1)
 	}
 	if strings.Contains(latestHash, "\n") {
 		latestHash = strings.ReplaceAll(latestHash, "\n", "")
 	}
-	fmt.Println("latest hash:", latestHash)
+	println("latest hash:", latestHash)
 
 	isFound := false
 	for _, path := range flag.Args() {
 		hash, err := run(exec.Command(gitPath, "log", "-n", "1", "--pretty=format:%H", "--", path))
 		if err != nil {
-			fmt.Println("Output:", hash)
-			fmt.Println("Error getting git hash:", err)
+			println("Output:", hash)
+			println("Error getting git hash:", err)
 			os.Exit(1)
 		}
 		if strings.Contains(hash, "\n") {
 			hash = strings.ReplaceAll(hash, "\n", "")
 		}
-		fmt.Println(path, "hash:", hash)
+		println(path, "hash:", hash)
 		if strings.EqualFold(hash, latestHash) {
-			fmt.Println("TRUE:", path, "was updated, triggering cmd")
+			println("TRUE:", path, "was updated, triggering cmd")
 			isFound = true
 			break
 		}
 	}
 	if isFound {
+		fmt.Println("[runifnew] Running cmd:", cmd)
 		result, err := run(exec.Command(cmdPath, cmdArgs...))
 		if err != nil {
-			fmt.Println("Output:", result)
-			fmt.Println("Error running cmd:", err)
+			println("Output:", result)
+			println("Error running cmd:", err)
 			os.Exit(1)
 		}
-		fmt.Println("Output:", result)
+		println("Output:", result)
 		return
 	}
 	if strings.ToLower(url) == "none" {
-		fmt.Println("FALSE: exiting gracefully no url set")
+		println("FALSE: exiting gracefully no url set")
 		return
 	}
-	fmt.Println("FALSE: Fetching url:", url)
+	println("FALSE: Fetching url:", url)
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error fetching url:", err)
+		println("Error fetching url:", err)
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		fmt.Println("Error fetching url, status code:", resp.StatusCode)
+		println("Error fetching url, status code:", resp.StatusCode)
 		os.Exit(1)
 	}
-	fmt.Println("Downloaded url:", url)
+	println("Downloaded url:", url)
 	f, err := os.Create(urlPath)
 	if err != nil {
-		fmt.Println("Error creating file:", err)
+		println("Error creating file:", err)
 		os.Exit(1)
 	}
 	defer f.Close()
 	_, err = io.Copy(f, resp.Body)
 	if err != nil {
-		fmt.Println("Error writing file:", err)
+		println("Error writing file:", err)
 		os.Exit(1)
 	}
-	fmt.Println("Saved url to:", urlPath)
+	fmt.Println("[runifnew] Saved url to:", urlPath)
 
 }
 
@@ -137,7 +144,7 @@ func usage() {
 }
 
 func run(cmd *exec.Cmd) (string, error) {
-	fmt.Println("Executing command:", strings.Join(cmd.Args, " "))
+	println("Executing command:", strings.Join(cmd.Args, " "))
 	cmd.Env = os.Environ()
 
 	buf := &bytes.Buffer{}
@@ -148,4 +155,11 @@ func run(cmd *exec.Cmd) (string, error) {
 		return buf.String(), err
 	}
 	return buf.String(), nil
+}
+
+func println(a ...interface{}) {
+	if isVerbose {
+		fmt.Printf("[runifnew] ")
+		fmt.Println(a...)
+	}
 }
